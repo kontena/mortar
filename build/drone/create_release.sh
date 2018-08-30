328111e7b64b4a106d811c2cb1e62a8841479871
Jussi Nummelin <jnummelin@users.noreply.github.com>
2018-08-30 15:02:58 +0300

    add binary building (#16)

---
+++ build/drone/create_release.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -ue
+
+apt-get update -y
+apt-get install -y -q curl bzip2
+
+# ship to github
+curl -sL https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2 | tar -xjO > /usr/local/bin/github-release
+chmod +x /usr/local/bin/github-release
+
+if [[ $DRONE_TAG =~ .+-.+ ]]; then
+    /usr/local/bin/github-release release \
+        --user kontena \
+        --repo mortar \
+        --tag $DRONE_TAG \
+        --name $DRONE_TAG \
+        --description "Pre-release, only for testing" \
+        --draft \
+        --pre-release
+else
+    /usr/local/bin/github-release release \
+        --user kontena \
+        --repo mortar \
+        --draft \
+        --tag $DRONE_TAG \
+        --name $DRONE_TAG
+fi
---
+++ build/travis/macos.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -ue
+
+brew install squashfs
+curl -sL https://dl.bintray.com/kontena/ruby-packer/0.5.0-dev/rubyc-darwin-amd64.gz | gunzip > /usr/local/bin/rubyc
+chmod +x /usr/local/bin/rubyc
+version=${TRAVIS_TAG#"v"}
+package="mortar-darwin-amd64-${version}"
+rubyc -o $package mortar
+./$package version
+
+# ship to github
+curl -sL https://github.com/aktau/github-release/releases/download/v0.7.2/darwin-amd64-github-release.tar.bz2 | tar -xjO > /usr/local/bin/github-release
+chmod +x /usr/local/bin/github-release
+/usr/local/bin/github-release upload \
+    --user kontena \
+    --repo mortar \
+    --tag $TRAVIS_TAG \
+    --name $package \
+    --file ./$package
+
+mkdir -p upload
+mv $package upload/
